1db1f261d70836f5c5393d630da9a85e7583ea45
MilkdevNew <93629950+MilkdevNew@users.noreply.github.com>
2022-10-24 17:43:49 -0400

    Initial commit

---
+++ Lobby/Main.client.lua
@@ -0,0 +1,12 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+--[[
+    Date: 10/24/2022
+    Time: 16:30:17
+
+    Path: Lobby\Main.client.lua
+
+    Script made by Gamerboy72008
+]]
