08a5f735a7352c16b36f0f8f4ee59a20d31bf213
MilkdevNew <93629950+MilkdevNew@users.noreply.github.com>
2022-10-26 22:00:05 -0400

    Camera

---
+++ client/Game/Core/Camera.client.lua
@@ -0,0 +1,74 @@
+repeat
+	task.wait()
+until game:IsLoaded()
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local ContextActionService = game:GetService("ContextActionService")
+local UserInputService = game:GetService("UserInputService")
+local Fusion = require(ReplicatedStorage.Common.Packages.Fusion)
+local Grain = require(ReplicatedStorage.Common.Utils.Grain)
+local New, Children, OnEvent, Value, Spring, Computed, Tween, Sping, Ref, Hydrate =
+	Fusion.New,
+	Fusion.Children,
+	Fusion.OnEvent,
+	Fusion.Value,
+	Fusion.Spring,
+	Fusion.Computed,
+	Fusion.Tween,
+	Fusion.Spring,
+	Fusion.Ref,
+	Fusion.Hydrate
+
+--[[
+    Date: 10/26/2022
+    Time: 20:36:38
+
+    Path: client\Game\Core\Camera.client.lua
+
+    Script made by Gamerboy72008
+]]
+local Action = "Camera"
+local FovValue = Value(70)
+local Camera = Hydrate(game.Workspace.CurrentCamera)({
+	FieldOfView = Tween(FovValue, TweenInfo.new(0.001, Enum.EasingStyle.Linear, Enum.EasingDirection.In)),
+})
+
+local max = 20
+local camera = false
+
+coroutine.resume(coroutine.create(function()
+	while true do
+		if camera then
+			if UserInputService:IsKeyDown(Enum.KeyCode.E) then
+				FovValue:set(FovValue:get() - 1)
+				if FovValue:get() < max then
+					FovValue:set(max)
+				end
+			end
+
+			if UserInputService:IsKeyDown(Enum.KeyCode.R) then
+				FovValue:set(FovValue:get() + 1)
+				if FovValue:get() > 70 then
+					FovValue:set(70)
+				end
+			end
+		end
+		task.wait()
+	end
+end))
+
+local function toggle(actionName, inputStat)
+	if inputStat == Enum.UserInputState.Begin then
+		if camera then
+			FovValue:set(70)
+		end
+		camera = not camera
+		Grain:bool(camera)
+		warn("State:", camera)
+	end
+end
+
+ContextActionService:BindAction(Action, toggle, true, Enum.KeyCode.B)
+ContextActionService:SetImage(Action, "rbxassetid://10723376471")
+ContextActionService:SetPosition(Action, UDim2.fromOffset(10, 30))
---
+++ shared/Utils/Grain.lua
@@ -0,0 +1,79 @@
+--[=[
+	
+:::::::::: ::::::::::: :::        ::::    ::::    ::::::::  :::::::::      :::     ::::::::::: ::::    ::: 
+:+:            :+:     :+:        +:+:+: :+:+:+  :+:    :+: :+:    :+:   :+: :+:       :+:     :+:+:   :+: 
++:+            +:+     +:+        +:+ +:+:+ +:+  +:+        +:+    +:+  +:+   +:+      +:+     :+:+:+  +:+ 
+:#::+::#       +#+     +#+        +#+  +:+  +#+  :#:        +#++:++#:  +#++:++#++:     +#+     +#+ +:+ +#+ 
++#+            +#+     +#+        +#+       +#+  +#+   +#+# +#+    +#+ +#+     +#+     +#+     +#+  +#+#+# 
+#+#            #+#     #+#        #+#       #+#  #+#    #+# #+#    #+# #+#     #+#     #+#     #+#   #+#+# 
+###        ########### ########## ###       ###   ########  ###    ### ###     ### ########### ###    #### 
+
+
+by boatbomber, 2019
+
+--]=]
+-- Put this as a LocalScript and tweak the settings to your liking.
+
+-- Settings
+local module = {}
+
+local GRAIN_SIZE = 50 -- A number 1 to 100
+local GRAIN_SPEED = 50 -- A number 1 to 100
+local GRAIN_VISIBILITY = 15 -- A number 1 to 100
+repeat
+	task.wait()
+until game:IsLoaded()
+
+local udim2 = UDim2.new
+local rand = math.random
+local plr = game.Players.LocalPlayer
+local plrGui = plr:WaitForChild("PlayerGui")
+
+local baseSize = 300 * (GRAIN_SIZE * 0.02)
+local speed = 1 / (40 * (GRAIN_SPEED * 0.02))
+local vis = 1.05 - (GRAIN_VISIBILITY * 0.01)
+
+local GrainGui = Instance.new("ScreenGui")
+GrainGui.Name = "FilmGrain"
+GrainGui.IgnoreGuiInset = true
+GrainGui.DisplayOrder = 99
+GrainGui.Parent = plrGui
+
+task.wait(1)
+
+local GrainImage = Instance.new("ImageLabel")
+GrainImage.Size = udim2(1, 0, 1, 0)
+GrainImage.BackgroundTransparency = 1
+GrainImage.ImageTransparency = vis
+GrainImage.ScaleType = Enum.ScaleType.Tile
+GrainImage.Visible = false
+GrainImage.Image = "http://www.roblox.com/asset/?id=28756351"
+GrainImage.Parent = GrainGui
+
+local last = 0
+
+function module:on()
+	GrainImage.Visible = true
+end
+
+function module:off()
+	GrainImage.Visible = false
+end
+
+function module:bool(bool)
+	GrainImage.Visible = bool
+end
+
+coroutine.resume(coroutine.create(function()
+	game:GetService("RunService").Heartbeat:Connect(function()
+		if tick() - last < speed then
+			return
+		end
+
+		last = tick()
+		GrainImage.TileSize =
+			udim2(rand(baseSize * 0.89, baseSize * 1.11) / 1000, 0, rand(baseSize * 0.89, baseSize * 1.11) / 1000, 0)
+	end)
+end))
+
+return module
