7c5bf4823178f2350fb8e1b6be11382aec5199d7
MilkdevNew <93629950+MilkdevNew@users.noreply.github.com>
2022-10-29 00:20:41 -0400

    sleeply

--- client/Lobby/UI/Main.client.lua
+++ client/Lobby/UI/Main.client.lua
@@ -9,6 +9,9 @@ local Fusion = require(ReplicatedStorage.Common.Packages.Fusion)
 local Fusion = require(ReplicatedStorage.Common.Packages.Fusion)
 local New, Children, OnEvent, Value, Spring, Computed, Tween, Ref =
 	Fusion.New, Fusion.Children, Fusion.OnEvent, Fusion.Value, Fusion.Spring, Fusion.Computed, Fusion.Tween, Fusion.Ref
+
+local ForPairs = Fusion.ForPairs
+local ForValues = Fusion.ForValues
 local Fliter = require(ReplicatedStorage.Common.Utils.Fliter)
 local DeviceChecker = require(ReplicatedStorage.Common.Utils.DeviceChecker)
 
@@ -25,6 +28,7 @@ local DeviceChecker = require(ReplicatedStorage.Common.Utils.DeviceChecker)
 local Padding = require(ReplicatedStorage.Common.UI.Utils.Padding)
 local Button = require(ReplicatedStorage.Common.UI.Button)
 local Frame = require(ReplicatedStorage.Common.UI.ItemFrame)
+local SimpleButton = require(ReplicatedStorage.Common.UI.ButtonSimple)
 -- Main
 
 Fliter:ApplyFilter("Vintage")
@@ -51,6 +55,82 @@ local Folder = Value()
 local GameFrame = Value()
 local SettingsFrame = Value()
 
+local data = Value(ReplicatedStorage.Games:GetChildren())
+
+local bo = Computed(function()
+	local out = {}
+	for index, value in data:get() do
+		out[index] = New("Frame")({
+			Name = "Frame",
+			BackgroundColor3 = Color3.fromRGB(72, 144, 120),
+			Position = UDim2.fromScale(0.0147, 0.000189),
+			Size = UDim2.fromScale(0.993, 0.00157),
+			ZIndex = 2,
+
+			[Children] = {
+				New("TextLabel")({
+					Name = "TextLabel",
+					FontFace = Font.new("rbxasset://fonts/families/Inconsolata.json"),
+					Text = value.Players.Value,
+					TextColor3 = Color3.fromRGB(255, 255, 255),
+					TextScaled = true,
+					TextSize = 14,
+					TextWrapped = true,
+					TextXAlignment = Enum.TextXAlignment.Left,
+					BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+					BackgroundTransparency = 1,
+					Position = UDim2.fromScale(0, 0.0476),
+					Size = UDim2.fromScale(0.631, 0.88),
+					ZIndex = 2,
+				}),
+
+				New("UIPadding")({
+					Name = "UIPadding",
+					PaddingBottom = UDim.new(0, 2),
+					PaddingLeft = UDim.new(0, 2),
+					PaddingRight = UDim.new(0, 2),
+					PaddingTop = UDim.new(0, 2),
+				}),
+
+				New("TextButton")({
+					Name = "TextButton",
+					FontFace = Font.new("rbxasset://fonts/families/Inconsolata.json"),
+					Text = "Join",
+					TextColor3 = Color3.fromRGB(255, 255, 255),
+					TextScaled = true,
+					TextSize = 14,
+					TextWrapped = true,
+					BackgroundColor3 = Color3.fromRGB(48, 184, 168),
+					Position = UDim2.fromScale(0.8, 0.0476),
+					Size = UDim2.fromScale(0.133, 0.88),
+					ZIndex = 2,
+
+					[Children] = {
+						New("UICorner")({
+							Name = "UICorner",
+						}),
+					},
+				}),
+
+				New("UICorner")({
+					Name = "UICorner",
+				}),
+			},
+		})
+	end
+	return out
+end)
+
+local update = function()
+	while true do
+		data:set(ReplicatedStorage.Games:GetChildren())
+		task.wait()
+	end
+end
+
+local co = coroutine.create(update)
+coroutine.resume(co)
+
 local GUI = New("ScreenGui")({
 	Name = "Select",
 	IgnoreGuiInset = true,
@@ -76,12 +156,41 @@ local GUI = New("ScreenGui")({
 							Name = "Game",
 							[Ref] = GameFrame,
 							[Children] = {
-								New("TextButton")({
-									Name = "Hello",
-									Size = UDim2.fromOffset(200, 50),
-									Text = "Click to play",
-									[OnEvent("MouseButton1Click")] = function()
-										game:GetService("TeleportService"):Teleport(11371061130)
+								New("ScrollingFrame")({
+									Name = "ScrollingFrame",
+									CanvasSize = UDim2.fromScale(0, 30),
+									ScrollBarImageColor3 = Color3.fromRGB(0, 0, 0),
+									Active = true,
+									BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+									BackgroundTransparency = 1,
+									BorderSizePixel = 0,
+									Position = UDim2.fromScale(0, 5.76e-08),
+									Size = UDim2.fromScale(1, 0.84),
+									ZIndex = 2,
+									[Children] = {
+										New("Frame")({
+											BackgroundTransparency = 1,
+											Size = UDim2.new(1, 0, 1, 0),
+											[Children] = {
+												Padding({
+													Number = 2,
+												}),
+												New("UIListLayout")({
+													Name = "UIListLayout",
+													Padding = UDim.new(0, 2),
+													SortOrder = Enum.SortOrder.LayoutOrder,
+												}),
+												bo,
+											},
+										}),
+									},
+								}),
+								SimpleButton({
+									Size = UDim2.fromScale(0.165, 0.0682),
+									Position = UDim2.fromScale(0.835, 0.853),
+									Text = "Create",
+									Function = function()
+										print("Pressed")
 									end,
 								}),
 							},
---
+++ shared/UI/ButtonSimple.lua
@@ -0,0 +1,90 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+local Fusion = require(ReplicatedStorage.Common.Packages.Fusion)
+local New, Children, OnEvent, Value, Spring, Computed, Tween =
+	Fusion.New, Fusion.Children, Fusion.OnEvent, Fusion.Value, Fusion.Spring, Fusion.Computed, Fusion.Tween
+local Padding = require(ReplicatedStorage.Common.UI.Utils.Padding)
+local MouseOverModule =
+	require(ReplicatedStorage:WaitForChild("Common"):WaitForChild("Utils"):WaitForChild("MouseEvent"))
+--[[
+    Date: 10/24/2022
+    Time: 22:02:59
+
+    Path: shared\UI\Button.lua
+
+    Script made by Gamerboy72008
+]]
+local Hover = ReplicatedStorage:WaitForChild("Assets"):WaitForChild("Audio"):WaitForChild("Hover")
+local Click = ReplicatedStorage:WaitForChild("Assets"):WaitForChild("Audio"):WaitForChild("Click")
+local info = TweenInfo.new(0.1, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut, 1)
+
+local function e(props)
+	local stroke = Value(1)
+	local bool = true
+
+	local UI = New("TextButton")({
+		Name = "TextButton",
+		FontFace = Font.new("rbxasset://fonts/families/Inconsolata.json"),
+		TextColor3 = Color3.fromRGB(255, 255, 255),
+		TextScaled = true,
+		Text = props.Text,
+		TextWrapped = true,
+		BackgroundColor3 = Color3.fromRGB(72, 128, 112),
+		BorderSizePixel = 0,
+		Size = props.Size,
+		Position = props.Position,
+
+		ZIndex = 2,
+
+		[Children] = {
+			New("UIPadding")({
+				Name = "UIPadding",
+				PaddingBottom = UDim.new(0, 2),
+				PaddingLeft = UDim.new(0, 1),
+				PaddingRight = UDim.new(0, 1),
+				PaddingTop = UDim.new(0, 2),
+			}),
+
+			New("UICorner")({
+				Name = "UICorner",
+				CornerRadius = UDim.new(0, 2),
+			}),
+
+			New("UIStroke")({
+				Name = "UIStroke",
+				ApplyStrokeMode = Enum.ApplyStrokeMode.Border,
+				Color = Color3.fromRGB(255, 255, 255),
+				Enabled = true,
+				Thickness = 2,
+				Transparency = Tween(stroke, info),
+				Archivable = false,
+			}),
+		},
+		-- [OnEvent("MouseEnter")] = function()
+		-- 	stroke:set(0)
+		-- 	Hover:Play()
+		-- end,
+		-- [OnEvent("MouseLeave")] = function()
+		-- 	stroke:set(1)
+
+		[OnEvent("MouseButton1Click")] = props.Function,
+		[OnEvent("MouseButton1Click")] = function()
+			Click:Play()
+		end,
+	})
+
+	local MouseEnter, MouseLeave = MouseOverModule.MouseEnterLeaveEvent(UI)
+
+	MouseEnter:Connect(function()
+		stroke:set(0)
+		Hover:Play()
+	end)
+
+	MouseLeave:Connect(function()
+		stroke:set(1)
+	end)
+
+	return UI
+end
+
+return e
